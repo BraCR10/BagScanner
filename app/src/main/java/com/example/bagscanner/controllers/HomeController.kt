12c2dd106600365e99f1823d1e6a5a5444aec75b
Brian Ramirez <Brianramirez01arias@gmail.com>
2025-03-11 15:49:40 -0600

    Configuring Home Screen Controller

--- app/src/main/java/com/example/bagscanner/controllers/HomeController.kt
+++ app/src/main/java/com/example/bagscanner/controllers/HomeController.kt
@@ -1,4 +1,23 @@
 package com.example.bagscanner.controllers
 
-class HomeController {
+
+import androidx.compose.runtime.Composable
+import androidx.lifecycle.ViewModel
+import com.example.bagscanner.enums.Screen
+// 2. Project-specific imports
+import com.example.bagscanner.views.HomeView
+
+class HomeController : ViewModel() {
+    // Attributes
+
+    //Methods
+
+    @Composable
+    fun DisplayScreen() {
+        HomeView(this)
+    }
+
+    fun navigateTo(screen: Screen){
+
+    }
 }
